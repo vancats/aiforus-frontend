62904af3dbf41c900f9c252121b1ea319a839e92
vancats <leiqifan@163.com>
2023-05-30 21:24:12 +0800

    chore: add a link to outside

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <div text-center title-brief py-4>
+  <div text-center title-brief py-4 cursor @click="linkToOutside">
     备案号：京ICP备2021032068号-2
   </div>
 </template>
+
+<script setup lang='ts'>
+const linkToOutside = () => {
+  window.open('https://beian.miit.gov.cn/')
+}
+</script>
